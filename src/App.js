a2f74893353425b712201cb942391d0ee0b69be2
MrCaro <36524539+MrCaro@users.noreply.github.com>
2021-02-12 16:37:43 -0500

    edit star component

--- src/App.js
+++ src/App.js
@@ -58,11 +58,11 @@ function App() {
             <CustomBoxMesh position={[0, 1, 0]} args={[3, 2, 1]} color='lightblue' speed={1}/>
             <CustomBoxMesh position={[5, 1, -2]} color='pink' speed={6}/>
             <Stars
-                radius={100} // Radius of the inner sphere (default=100)
-                depth={50} // Depth of area where stars should fit (default=50)
-                count={5000} // Amount of stars (default=5000)
-                factor={4} // Size factor (default=4)
-                saturation={0} // Saturation 0-1 (default=0)
+                radius={100} 
+                depth={50} 
+                count={5000} 
+                factor={4} 
+                saturation={0} 
             />
             <OrbitControls/>
         </Canvas>
