a4bf6f379c5348ff5886b0852af1977413a21b39
MrCaro <36524539+MrCaro@users.noreply.github.com>
2021-02-14 18:43:04 -0500

    adding glb model

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 
-import { useRef } from 'react';
+import { useRef, Suspense } from 'react';
+
+import { Canvas, useFrame, useLoader } from 'react-three-fiber';
+import { softShadows, MeshWobbleMaterial, OrbitControls, Stars, useGLTF } from '@react-three/drei';
+
 import './App.scss';
-
-import { Canvas, useFrame } from 'react-three-fiber';
-
-import { softShadows, MeshWobbleMaterial, OrbitControls, Stars} from '@react-three/drei';
+import Model from './3d/NOVELO_EARTH.glb';
 
 softShadows();
+
+const ModelCustom = (props) => {
+    const group = useRef(null);
+    const { nodes, materials } = useGLTF(Model);
+    const scaleValue = 0.03;
+
+    useFrame(() => (group.current.rotation.y += 0.001));
+
+    return (
+        <group ref={group} {...props} dispose={null}>
+            <mesh scale={[scaleValue, scaleValue, scaleValue]} material={materials['02___Default']} geometry={nodes['buffer-0-mesh-0'].geometry} />
+            <mesh castShadow scale={[scaleValue, scaleValue, scaleValue]} material={materials.Mat} geometry={nodes['buffer-0-mesh-0_1'].geometry} />
+        </group>
+    )
+}
 
 const CustomBoxMesh = ({ position, args, color, speed }) => {
     const mesh = useRef(null);
@@ -25,13 +41,24 @@ const CustomBoxMesh = ({ position, args, color, speed }) => {
     )
 }
 
+const Plane = () => {
+    return (
+        <group>
+            <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -3, 0]}>
+                <planeBufferGeometry attach='geometry' args={[50, 50]}/>
+                <shadowMaterial attach='material' opacity={.3} />
+            </mesh>
+        </group>
+    )
+}
+
 function App() {
     return (
     <>
         <Canvas
             shadowMap
             colorManagement
-            camera={{position: [-5, 2, 10], fov: 70}}
+            camera={{position: [-5, 20, 50], fov: 70}}
         >
             <ambientLight intensity={.5} />
             <directionalLight
@@ -46,17 +73,14 @@ function App() {
                 shadow-camera-top={10}
                 shadow-camera-bottom={-10}
             />
+            <Suspense fallback={null}>
+                <ModelCustom />
+            </Suspense>
             <pointLight position={[-10, 0, -20]} intensity={.5}/>
             <pointLight position={[0, -10, 0]} intensity={1.5}/>
-            <group>
-                <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -3, 0]}>
-                    <planeBufferGeometry attach='geometry' args={[50, 50]}/>
-                    <shadowMaterial attach='material' opacity={.3} />
-                </mesh>
-            </group>
-            <CustomBoxMesh position={[-2, 1, -5]} color='yellow' speed={6}/>
-            <CustomBoxMesh position={[0, 1, 0]} args={[3, 2, 1]} color='lightblue' speed={1}/>
-            <CustomBoxMesh position={[5, 1, -2]} color='pink' speed={6}/>
+            <CustomBoxMesh position={[-20, 25, -5]} color='green' speed={4}/>
+            <CustomBoxMesh position={[35, 15, -20]} args={[5, 10, 10]} color='lightgray' speed={1}/>
+            <CustomBoxMesh position={[12, 10, 11]} color='red' speed={4}/>
             <Stars
                 radius={100} 
                 depth={50} 
