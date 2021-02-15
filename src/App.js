003190f29e874e87217686f8c98fd1d6b925ec17
MrCaro <36524539+MrCaro@users.noreply.github.com>
2021-02-15 16:16:31 -0500

    adding planet data and click states

--- src/App.js
+++ src/App.js
@@ -1,64 +1,58 @@
 
-import { useRef, Suspense } from 'react';
+import { useRef, Suspense, useState } from 'react';
 
-import { Canvas, useFrame, useLoader } from 'react-three-fiber';
-import { softShadows, MeshWobbleMaterial, OrbitControls, Stars, useGLTF } from '@react-three/drei';
+import { Canvas, useFrame } from 'react-three-fiber';
+import { softShadows, OrbitControls, Stars, useGLTF } from '@react-three/drei';
+import { useSpring, a } from 'react-spring/three';
 
 import './App.scss';
+import PlanetsData from './data/planets.json';
 import Model from './3d/NOVELO_EARTH.glb';
 
 softShadows();
 
-const ModelCustom = (props) => {
+const PlanetEarth = (props) => {
     const group = useRef(null);
     const { nodes, materials } = useGLTF(Model);
-    const scaleValue = 0.03;
+    const scaleValue = [0.03, 0.03, 0.03];
+
+    const [expand, setExpand] = useState(false);
+
+    const propClick = useSpring({
+        scale: expand ? [0.06, 0.06, 0.06] : scaleValue
+    });
 
     useFrame(() => (group.current.rotation.y += 0.001));
 
     return (
-        <group ref={group} {...props} dispose={null}>
-            <mesh scale={[scaleValue, scaleValue, scaleValue]} material={materials['02___Default']} geometry={nodes['buffer-0-mesh-0'].geometry} />
-            <mesh castShadow scale={[scaleValue, scaleValue, scaleValue]} material={materials.Mat} geometry={nodes['buffer-0-mesh-0_1'].geometry} />
+        <group ref={group} {...props} dispose={null} >
+            <a.mesh onClick={() => setExpand(!expand)} scale={propClick.scale} material={materials['02___Default']} geometry={nodes['buffer-0-mesh-0'].geometry} />
+            <a.mesh onClick={() => setExpand(!expand)} scale={propClick.scale} material={materials.Mat} geometry={nodes['buffer-0-mesh-0_1'].geometry} />
         </group>
     )
 }
 
-const CustomBoxMesh = ({ position, args, color, speed }) => {
-    const mesh = useRef(null);
-    useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
-
+const DisplayPlanetInfo = (props) => {                                                                   
     return (
-        <mesh castShadow ref={mesh} position={position}>
-            <boxBufferGeometry attach='geometry' args={args} />
-            <MeshWobbleMaterial
-                attach='material'
-                color={color}
-                speed={speed}
-                factor={0.3}
-            />
-        </mesh>
-    )
-}
-
-const Plane = () => {
-    return (
-        <group>
-            <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -3, 0]}>
-                <planeBufferGeometry attach='geometry' args={[50, 50]}/>
-                <shadowMaterial attach='material' opacity={.3} />
-            </mesh>
-        </group>
-    )
+        <div className={props.display}>
+            <div className="planet-info-wrapper">
+                <h1>{props.planet.name}</h1>
+                <p>{props.planet.info}</p>
+            </div>
+        </div>
+    );
 }
 
 function App() {
+    const [showPlanetInfo, setShowPlanetInfo] = useState('');
+    const [visibilityPlanetInfo, setVisibilityPlanetInfo] = useState(false);
+
     return (
     <>
         <Canvas
             shadowMap
             colorManagement
-            camera={{position: [-5, 20, 50], fov: 70}}
+            camera={{position: [10, 50, 50], fov: 70}}
         >
             <ambientLight intensity={.5} />
             <directionalLight
@@ -73,14 +67,17 @@ function App() {
                 shadow-camera-top={10}
                 shadow-camera-bottom={-10}
             />
-            <Suspense fallback={null}>
-                <ModelCustom />
-            </Suspense>
             <pointLight position={[-10, 0, -20]} intensity={.5}/>
             <pointLight position={[0, -10, 0]} intensity={1.5}/>
-            <CustomBoxMesh position={[-20, 25, -5]} color='green' speed={4}/>
-            <CustomBoxMesh position={[35, 15, -20]} args={[5, 10, 10]} color='lightgray' speed={1}/>
-            <CustomBoxMesh position={[12, 10, 11]} color='red' speed={4}/>
+            <Suspense fallback={null}>
+                {PlanetsData.map((planet, i) => (
+                    <PlanetEarth 
+                        key={i} 
+                        onClick={() => {setShowPlanetInfo(planet); setVisibilityPlanetInfo(!visibilityPlanetInfo)}} 
+                        position={planet.position}
+                    />
+                ))}
+            </Suspense>
             <Stars
                 radius={100} 
                 depth={50} 
@@ -88,8 +85,12 @@ function App() {
                 factor={4} 
                 saturation={0} 
             />
-            <OrbitControls/>
+            <OrbitControls />
         </Canvas>
+        <DisplayPlanetInfo 
+            display={visibilityPlanetInfo ? "" : "hide"}  
+            planet={showPlanetInfo} 
+        />
     </>
     );
 }
